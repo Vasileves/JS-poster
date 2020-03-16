676faa6873e5455c80aca22244f097a70562a1be
Vasileves <55274720+Vasileves@users.noreply.github.com>
2020-03-16 17:58:50 +0300

    Added spinning text
    But it's laggy as hell! Gonna find a lighter solution.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-let moveanim = anime({
-  targets: '.badtxt',
+let moveanim_s= anime({
+  targets: '.s',
   rotate: 360,
-  duration: 15000,
+  duration: 10000,
+  easing: 'linear',
+  loop: true
+});
+
+let moveanim_m = anime({
+  targets: '.m',
+  rotate: 360,
+  duration: 20000,
+  easing: 'linear',
+  loop: true
+});
+
+let moveanim_l = anime({
+  targets: '.l',
+  rotate: 360,
+  duration: 30000,
   easing: 'linear',
   loop: true
 });
