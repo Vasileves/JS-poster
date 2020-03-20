061b5a6713ddf2b4d2ea25d5c22976135fcd3c35
Vasileves <55274720+Vasileves@users.noreply.github.com>
2020-03-20 03:35:38 +0300

    Added wave weight animation
    Still too crappy and laggy

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-let moveanim_s= anime({
-  targets: '.s',
-  rotate: 360,
-  duration: 10000,
-  easing: 'linear',
-  loop: true
-});
+window.onload = function() {
+	lax.setup() // init
 
-let moveanim_m = anime({
-  targets: '.m',
-  rotate: 360,
-  duration: 20000,
-  easing: 'linear',
-  loop: true
-});
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
 
-let moveanim_l = anime({
-  targets: '.l',
-  rotate: 360,
-  duration: 30000,
-  easing: 'linear',
-  loop: true
-});
+	window.requestAnimationFrame(updateLax)
+}
+
+// let moveanim_s= anime({
+//   targets: '.s',
+//   rotate: 360,
+//   duration: 10000,
+//   easing: 'linear',
+//   loop: true
+// });
+//
+// let moveanim_m = anime({
+//   targets: '.m',
+//   rotate: 360,
+//   duration: 20000,
+//   easing: 'linear',
+//   loop: true
+// });
+//
+// let moveanim_l = anime({
+//   targets: '.l',
+//   rotate: 360,
+//   duration: 30000,
+//   easing: 'linear',
+//   loop: true
+// });
